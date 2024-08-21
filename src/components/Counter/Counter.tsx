9123f57675efe6194b2254d606594a6409230da5
filatow <not-found@inbox.ru>
2024-08-21 13:04:20 +0300

    Added settings section markup

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -15,30 +15,53 @@ const Counter: React.FC<CounterPropsType> = (props) => {
     const isLimitReached = value >= props.limit
     const isIncDisabled = isLimitReached
     const isResetDisabled = value === 0
-    const monitorClassName = `counter__monitor ${isLimitReached && 'text-alert'}`
+    const monitorClassName = `counter__monitor ${isLimitReached ? 'text-alert' : ''}`
 
     const incOnClickHandler = () => setValue(value + increment)
     const resetOnClickHandler = () => setValue(props.start)
+    const setOnClickHandler = () => {
+    }
 
     return (
         <div className="counter">
-            <div className={monitorClassName}>
-                {value}
-            </div>
-            <div className="counter__buttons">
-                <Button
-                    caption="INC"
-                    onClick={incOnClickHandler}
-                    classes="counter__button"
-                    disabled={isIncDisabled}
-                />
-                <Button
-                    caption="RESET"
-                    onClick={resetOnClickHandler}
-                    classes="counter__button"
-                    disabled={isResetDisabled}
-                />
-            </div>
+            <section className="counter__widget counter__settings">
+                <div className="counter__top counter__settings-form">
+                    <div className="input-group">
+                        <label className="input-group__label" htmlFor="max-value">Max value:</label>
+                        <input className="input-group__number" id="max-value" type="number"/>
+                    </div>
+                    <div className="input-group">
+                        <label className="input-group__label" htmlFor="start-value">Start value:</label>
+                        <input className="input-group__number" id="start-value" type="number"/>
+                    </div>
+                </div>
+                <div className="counter__buttons">
+                    <Button
+                        caption="SET"
+                        onClick={setOnClickHandler}
+                        classes="counter__button"
+                    />
+                </div>
+            </section>
+            <section className="counter__widget counter__main">
+                <div className={'counter__top ' + monitorClassName}>
+                    {value}
+                </div>
+                <div className="counter__buttons">
+                    <Button
+                        caption="INC"
+                        onClick={incOnClickHandler}
+                        classes="counter__button"
+                        disabled={isIncDisabled}
+                    />
+                    <Button
+                        caption="RESET"
+                        onClick={resetOnClickHandler}
+                        classes="counter__button"
+                        disabled={isResetDisabled}
+                    />
+                </div>
+            </section>
         </div>
     );
 }
